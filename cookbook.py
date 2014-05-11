e5eb980c4ba10c5d7ec65c083e1aa3ae23157397
Ben Osment <benjamin.j.osment@gmail.com>
2014-05-11 13:58:12 -0400

    added flask-script, requirements.txt

--- cookbook.py
+++ cookbook.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask.ext.script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -11,5 +14,5 @@ def recipe(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
 
