0685a45e72a3dfe54e0344b82b40e91ce627bb16
Ben Osment <benjamin.j.osment@gmail.com>
2014-06-15 10:30:22 -0400

    added prep time, num portions, source, date created and updated to DB model

--- app/models.py
+++ app/models.py
@@ -1,14 +1,21 @@
 from . import db
-
+from datetime import datetime
 
 # With an ORM, the model (a Python class) is mapped to
 # columns in a corresponding database table
 class Recipe(db.Model):
     __tablename__ = 'recipes'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
     description = db.Column(db.Text(100))
     directions = db.Column(db.Text(1000))
+    ingredients = db.Column(db.Text(1000))
+    preparation_time = db.Column(db.String(20))
+    num_portions = db.Column(db.String(20))
+    source = db.Column(db.String(120))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+    date_updated = db.Column(db.DateTime, default=datetime.utcnow())
+    # TODO add image
     # one-to-many relationship from tags to recipes
     # TODO -- should be many-to-many
     tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'))
