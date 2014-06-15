9e28199d410b4505c758f7de7ae3f37df10123ac
Ben Osment <benjamin.j.osment@gmail.com>
2014-06-15 10:38:00 -0400

    misc whitespace fix

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from datetime import datetime
 
+
 # With an ORM, the model (a Python class) is mapped to
 # columns in a corresponding database table
 class Recipe(db.Model):
@@ -19,7 +20,7 @@ class Recipe(db.Model):
     # one-to-many relationship from tags to recipes
     # TODO -- should be many-to-many
     tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'))
-    
+
     def __repr__(self):
         return 'Recipe %r' % self.name
 
@@ -30,7 +31,7 @@ class Ingredient(db.Model):
     name = db.Column(db.String(64))
     quantity = db.Column(db.String(64))
     # TODO -- one-to-one relationship between Ingredients and Recipes
-    
+
     def __repr__(self):
         return 'Ingredient %r' % self.name
 
@@ -42,6 +43,6 @@ class Tag(db.Model):
     # one-to-many relationship from tags to recipes
     # TODO -- should be many-to-many
     recipes = db.relationship('Recipe', backref='tag')
-    
+
     def __repr__(self):
         return 'Tag %r' % self.name
