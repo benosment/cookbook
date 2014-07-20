7a9609407bcca4e5c41cc4753fbdf80e4bf20940
Ben Osment <benjamin.j.osment@gmail.com>
2014-07-20 10:13:50 -0400

    made main page an index of all recipes, added add_recipe functionality

--- app/main/views.py
+++ app/main/views.py
@@ -8,16 +8,7 @@ from ..email import send_email
 from datetime import datetime
 
 # TODO
-# - change index to show a list of all recipes
-## @app.route('/')
-## def show_recipes():
-##   recipes = Recipe.query.all()
-##   return render_template('show_recipes.html', recipes=recipes)
-# 
-# - move old index to add
-# - add a link for add on main page
-# - add all fields in the recipe into the form
-#  - in the form, have a diag file selector for the field
+# - clean up the Recipe constructor
 # - lowercase the title of recipe
 # - create an edit page
 ## def edit_profile(request):
@@ -30,8 +21,13 @@ from datetime import datetime
 ##        return redirect('/home')
 ##    return render_to_response('edit_profile.html', form=form)
 # - create a script which backups the database periodically
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
+   recipes = Recipe.query.all()
+   return render_template('index.html', recipes=recipes)
+
+@main.route('/add_recipe', methods=['GET', 'POST'])    
+def add_recipe():
     form = RecipeForm()
     if form.validate_on_submit():
         recipe = Recipe.query.filter_by(name=form.name.data).first()
@@ -53,12 +49,11 @@ def index():
                 send_email(current_app.config['COOKBOOK_ADMIN'],
                            'New recipe', 'mail/new_recipe',
                            recipe=recipe)
+            return redirect(url_for('.index'))                
         else:
             flash('That recipe has already been added')
-        session['rname'] = form.name.data
-        form.name.data = ''
-        # always end POSTs with a redirect
-        # this elimates issues when refreshing the page
-        return redirect(url_for('.index'))
-    return render_template("index.html", form=form,
-                           rname=session.get('rname'))
+            # always end POSTs with a redirect
+            # this elimates issues when refreshing the page
+            return redirect(url_for('.add_recipe'))
+    return render_template('add_recipe.html', form=form)
+    
